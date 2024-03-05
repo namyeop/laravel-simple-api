0409eec682da4e277a4f814f563115aed2c3cf76
Nam Yeop Lee <namyeop@Nams-MacBook-Pro.local>
2024-03-06 00:04:58 +0900

    feat: Refactor PersonalTrainerController and PersonalTrainerService

--- app/Http/Controllers/PersonalTrainerController.php
+++ app/Http/Controllers/PersonalTrainerController.php
@@ -3,7 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Validator;
+
+class PersonalSolution extends JsonResource
+{
+    public $solution;
+    public function __construct($solution)
+    {
+        $this->solution = $solution;
+    }
+
+    public function toArray(Request $request)
+    {
+        return [
+            'status' => '200',
+            'message' => 'Success',
+            /* @var string[] $solution*/
+            'solution' => $this->solution
+        ];
+    }
+}
+
 
 class PersonalTrainerController extends Controller
 {
@@ -11,7 +32,7 @@ class PersonalTrainerController extends Controller
      * Training Plan을 Return 합니다.
      * @param Request $request
      */
-    public function getTrainingPlan(Request $request)
+    public function getTrainingPlan(Request $request): PersonalSolution
     {
         $validator = Validator::make($request->all(), [
             'type' => 'string|in:DIET,FITNESS',
@@ -21,16 +42,20 @@ class PersonalTrainerController extends Controller
 
         if ($validator->fails()) {
             return response()->json([
-                'data' => $validator->errors(),
-                'message' => 'Error',
-                'status' => '422'
+                "data" => [
+                    'solution' => $validator->errors(),
+                    'message' => 'Parameter error',
+                    'status' => '422'
+                ]
             ], 422);
         }
 
         $validated = $validator->validated();
 
         $personalSolution = app('personalTrainer')->getPersonalSolution($validated);
-
-        return $personalSolution;
+        /**
+         * 맞춤형 솔루션을 응답합니다.
+         */
+        return new PersonalSolution($personalSolution);
     }
 }
--- app/Services/PersonalTrainerService.php
+++ app/Services/PersonalTrainerService.php
@@ -6,100 +6,100 @@ use Faker\Provider\ar_EG\Person;
 
 abstract class PersonalTrainer
 {
-  abstract public function getTrainingPlan(array $tags);
+    abstract public function getTrainingPlan(array $tags);
 }
 
 class DietExpert extends PersonalTrainer
 {
-  private $tagSolutions = [
-    "enough_time" => ["Intermittent Fasting"],
-    "strong_will" => ["Intermittent Fasting"],
-    "enough_money" => ["LCHF"]
-  ];
+    private $tagSolutions = [
+        "enough_time" => ["Intermittent Fasting"],
+        "strong_will" => ["Intermittent Fasting"],
+        "enough_money" => ["LCHF"]
+    ];
 
-  public function getTrainingPlan(array $tags)
-  {
-    $solutions = [];
-    foreach ($tags as $tag) {
-      if (isset($this->tagSolutions[$tag])) {
-        $solutions = array_merge($solutions, $this->tagSolutions[$tag]);
-      }
+    public function getTrainingPlan(array $tags)
+    {
+        $solutions = [];
+        foreach ($tags as $tag) {
+            if (isset($this->tagSolutions[$tag])) {
+                $solutions = array_merge($solutions, $this->tagSolutions[$tag]);
+            }
+        }
+        return array_unique($solutions);
     }
-    return array_unique($solutions);
-  }
 }
 
 class FitnessCoach extends PersonalTrainer
 {
-  private $tagSolutions = [
-    "enough_time" => ["Strength"],
-    "strong_will" => ["Crossfit", "Cardio Exercise", 'Strength'],
-    "enough_money" => ["Crossfit", "Spinning"]
-  ];
+    private $tagSolutions = [
+        "enough_time" => ["Strength"],
+        "strong_will" => ["Crossfit", "Cardio Exercise", 'Strength'],
+        "enough_money" => ["Crossfit", "Spinning"]
+    ];
 
-  public function getTrainingPlan(array $tags)
-  {
-    $solutions = [];
-    foreach ($tags as $tag) {
-      if (isset($this->tagSolutions[$tag])) {
-        $solutions = array_merge($solutions, $this->tagSolutions[$tag]);
-      }
+    public function getTrainingPlan(array $tags)
+    {
+        $solutions = [];
+        foreach ($tags as $tag) {
+            if (isset($this->tagSolutions[$tag])) {
+                $solutions = array_merge($solutions, $this->tagSolutions[$tag]);
+            }
+        }
+        return array_unique($solutions);
     }
-    return array_unique($solutions);
-  }
 }
 
 class PersonalTrainerFactory
 {
-  public static $trainers = [
-    'DIET' => DietExpert::class,
-    'FITNESS' => FitnessCoach::class,
-  ];
+    public static $trainers = [
+        'DIET' => DietExpert::class,
+        'FITNESS' => FitnessCoach::class,
+    ];
 
-  public static function make($type)
-  {
-    if (isset(self::$trainers[$type])) {
-      return new self::$trainers[$type];
-    } else {
-      return;
+    public static function make($type)
+    {
+        if (isset(self::$trainers[$type])) {
+            return new self::$trainers[$type];
+        } else {
+            return;
+        }
     }
-  }
 }
 
 class PersonalTrainerService
 {
-  /**
-   * @param array  $validatedParams 
-   * @return array
-   */
+    /**
+     * @param array  $validatedParams
+     * @return array
+     */
 
-  public function getPersonalSolution(array $validatedParams): array
-  {
-    $type = $validatedParams['type'] ?? null;
-    $tag = $validatedParams['tag'];
+    public function getPersonalSolution(array $validatedParams): array
+    {
+        $type = $validatedParams['type'] ?? null;
+        $tag = $validatedParams['tag'];
 
-    $originalSolution = ['Intermittent Fasting', 'LCHF', 'Strength', 'Crossfit', 'Cardio Exercise', 'Spinning'];
+        $originalSolution = ['Intermittent Fasting', 'LCHF', 'Strength', 'Crossfit', 'Cardio Exercise', 'Spinning'];
 
-    if ($type) {
-      $personalTrainer = PersonalTrainerFactory::make($type);
-      $solution = $personalTrainer->getTrainingPlan($tag);
+        if ($type) {
+            $personalTrainer = PersonalTrainerFactory::make($type);
+            $solution = $personalTrainer->getTrainingPlan($tag);
 
-      $differedSolution = array_diff($originalSolution, $solution);
+            $differedSolution = array_diff($originalSolution, $solution);
 
-      $result = array_merge($solution, $differedSolution);
+            $result = array_merge($solution, $differedSolution);
 
-      return $solution;
-    } else {
-      $solutions = [];
-      foreach (PersonalTrainerFactory::$trainers as $trainerClass) {
-        $trainer = new $trainerClass;
-        $solutions = array_merge($solutions, $trainer->getTrainingPlan($tag));
+            return $solution;
+        } else {
+            $solutions = [];
+            foreach (PersonalTrainerFactory::$trainers as $trainerClass) {
+                $trainer = new $trainerClass;
+                $solutions = array_merge($solutions, $trainer->getTrainingPlan($tag));
 
-        $differedSolution = array_diff($originalSolution, $solutions);
+                $differedSolution = array_diff($originalSolution, $solutions);
 
-        $result = array_merge($solutions, $differedSolution);
-      }
-      return array_unique($result);
+                $result = array_merge($solutions, $differedSolution);
+            }
+            return array_unique($result);
+        }
     }
-  }
 }
